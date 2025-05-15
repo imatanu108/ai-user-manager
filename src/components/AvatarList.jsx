ba99e922a7bd09e333292e997d229c18d6e5ba76
Atanu Hazra <imatanuhazra@gmail.com>
2025-05-15 14:18:37 +0530

    add loading

--- src/components/AvatarList.jsx
+++ src/components/AvatarList.jsx
@@ -1,10 +1,33 @@
 'use client';
 
+import { useEffect, useState } from "react";
 import useUsersInfo from "@/hooks/useUsersInfo";
 import AvatarCard from "./AvatarCard";
 
 const AvatarList = () => {
-    const users = useUsersInfo().slice(0, 3)
+    const [loading, setLoading] = useState(true);
+    const [users, setUsers] = useState([]);
+
+    const fetchedUsers = useUsersInfo();
+
+    useEffect(() => {
+        if (Array.isArray(fetchedUsers) && fetchedUsers.length > 0) {
+            setUsers(fetchedUsers.slice(0, 3));
+            setLoading(false);
+        } else {
+            setUsers([]);
+            setLoading(false);
+        }
+    }, [fetchedUsers]);
+
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center h-64 text-cyan-200 font-medium text-lg animate-pulse">
+                Loading avatars...
+            </div>
+        );
+    }
+
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mx-8 mt-12 mb-30">
             {users.map((user) => (
